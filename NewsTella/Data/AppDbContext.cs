77bf784d5f68955a114878d1b2714caf68a69955
marlind24 <marika.lindqvist2024@outlook.com>
2024-05-13 12:59:38 +0200

    Add project files.

---
+++ NewsTella/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using NewsTella.Models.Database;
+
+namespace NewsTella.Data
+{
+	public class AppDbContext : IdentityDbContext<User>
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options)
+			: base(options)
+		{
+		}
+	}
+}
---
+++ NewsTella/Models/Database/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace NewsTella.Models.Database
+{
+	public class User:IdentityUser
+
+	{
+		public string FirstName {  get; set; } = string.Empty;
+		public string LastName { get; set; } = string.Empty;
+	}
+}
