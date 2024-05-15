4983d2954bb98dd150b9f82a29c3b98d6877ad91
nadee <nadee@NADEE>
2024-05-15 22:10:37 +0200

    created dummy users, menu  item for user roles

---
+++ NewsTella/Controllers/SubscriptionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NewsTella.Controllers
+{
+    public class SubscriptionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
