8206f1cc26b321fed280da5d75b4e6bf40957c8d
marlind24 <marika.lindqvist2024@outlook.com>
2024-06-07 12:55:58 +0200

    // If else statment for Writer- and Editor-Roles added in the article Index view and detailed/read view.

--- NewsTella/Services/ArticlesService.cs
+++ NewsTella/Services/ArticlesService.cs
@@ -8,106 +8,123 @@ using NewsTella.Migrations;
 
 namespace NewsTella.Services
 {
-    public class ArticlesService : IArticlesService
-    {
-        private readonly AppDbContext _db;
-        private readonly IConfiguration _configuration;
+	public class ArticlesService : IArticlesService
+	{
+		private readonly AppDbContext _db;
+		private readonly IConfiguration _configuration;
 
-        public ArticlesService(AppDbContext db, IConfiguration configuration)
-        {
-            _db = db;
-            _configuration = configuration;
-        }
+		public ArticlesService(AppDbContext db, IConfiguration configuration)
+		{
+			_db = db;
+			_configuration = configuration;
+		}
 
-        public void AddArticle(Article article)
-        {            
-            _db.Articles.Add(article);
-            _db.SaveChanges();
-        }
-        public void UpdateArticle(Article article)
-        {
-            article.DateStamp = DateTime.Now;
-            article.Status = "Draft";
+		public void AddArticle(Article article)
+		{
+			_db.Articles.Add(article);
+			_db.SaveChanges();
+		}
+		public void UpdateArticle(Article article)
+		{
+			article.DateStamp = DateTime.Now;
+			article.Status = "Edited";
+			
+			_db.Articles.Update(article);
+			_db.SaveChanges();
+		}
+		public void DeleteArticle(Article article)
+		{
+			_db.Articles.Remove(article);
+			_db.SaveChanges();
+		}
 
-            _db.Articles.Update(article);
-            _db.SaveChanges();
-        }
-        public void DeleteArticle(Article article)
-        {
-            _db.Articles.Remove(article);
-            _db.SaveChanges();
-        }
+		public Article GetArticleById(int id)
+		{
+			var article = _db.Articles.Include(a => a.Categories).FirstOrDefault(a => a.Id == id);
+			return article;
+		}
+		public List<Article> GetArticles()
+		{
+			var article = _db.Articles.Where(a => a.IsDeleted == false).Include(a => a.Categories).ToList();
+			return article;
+		}
 
-        public Article GetArticleById(int id)
-        {
-            var article = _db.Articles.Include(a => a.Categories).FirstOrDefault(a => a.Id == id);
-            return article;
-        }
-        public List<Article> GetArticles()
-        {
-            var article = _db.Articles.Where(a => a.IsDeleted == false).Include(a => a.Categories).ToList();
-            return article;
-        }
+		public void UpdateArticleStatus(Article article)
+		{
+			article.DateStamp = DateTime.Now;
+			article.Status = article.Status;
 
-        public void UpdateArticleStatus(Article article)
-        {
-            article.DateStamp = DateTime.Now;
-            article.Status = "Published";
+			_db.Update(article);
+			_db.SaveChanges();
+		}
+		public void UpdateArticleStatusPublished(Article article)
+		{
+			article.DateStamp = DateTime.Now;
+			article.Status = "Published";
 
-            _db.Update(article);
-            _db.SaveChanges();
-        }
-        
+			_db.Update(article);
+			_db.SaveChanges();
 
-        public ICollection<Article> FindByCategory(string category)
-        {
-            return _db.Categories.Where(c => c.Name == category).FirstOrDefault().Articles.Where(a => a.IsDeleted == false).ToList();
-        }
+		}
+		public void UpdateArticleStatusSubmitted(Article article)
+		{
+			article.DateStamp = DateTime.Now;
+			article.Status = "Submitted";
 
-        public ICollection<Article> FindByHeadline(string headline)
-        {
-            return _db.Articles.Where(a => a.Headline.Contains(headline) && a.IsDeleted == false).ToList();
-        }
-        public async Task LikeArticleAsync(int id)
-        {
-            var article = await _db.Articles.FindAsync(id);
-            if (article == null)
-            {
-                throw new Exception("Article not found");
-            }
-            else if(article.Status =="Published")
-            {
-                article.Likes += 1;
-                _db.Update(article);
-                await _db.SaveChangesAsync();
-            }            
-        }
-        public async Task IncrementViewsAsync(int id)
-        {
-            var article = await _db.Articles.FindAsync(id);
-            if (article == null)
-            {
-                throw new Exception("Article not found");
-            }
+			_db.Update(article);
+			_db.SaveChanges();
+		}
 
-            article.Views += 1;
-            _db.Update(article);
-            await _db.SaveChangesAsync();
-        }
 
-        public Task GetArticleByIdAsync(int id, Article article)
-        {
-            throw new NotImplementedException();
-        }
+		public ICollection<Article> FindByCategory(string category)
+		{
+			return _db.Categories.Where(c => c.Name == category).FirstOrDefault().Articles.Where(a => a.IsDeleted == false).ToList();
+		}
 
-        public Task GetArticleByIdAsync()
-        {
-            throw new NotImplementedException();
-        }
+		public ICollection<Article> FindByHeadline(string headline)
+		{
+			return _db.Articles.Where(a => a.Headline.Contains(headline) && a.IsDeleted == false).ToList();
+		}
+		public async Task LikeArticleAsync(int id)
+		{
+			var article = await _db.Articles.FindAsync(id);
+			if (article == null)
+			{
+				throw new Exception("Article not found");
+			}
+			else if (article.Status == "Published")
+			{
+				article.Likes += 1;
+				_db.Update(article);
+				await _db.SaveChangesAsync();
+			}
+		}
+		public async Task IncrementViewsAsync(int id)
+		{
+			var article = await _db.Articles.FindAsync(id);
+			if (article == null)
+			{
+				throw new Exception("Article not found");
+			}
 
-        public Task<string?> GetArticleByIdAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
-    }
+			article.Views += 1;
+			_db.Update(article);
+			await _db.SaveChangesAsync();
+		}
+
+		public Task GetArticleByIdAsync(int id, Article article)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task GetArticleByIdAsync()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<string?> GetArticleByIdAsync(int id)
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
--- NewsTella/Services/IArticlesService.cs
+++ NewsTella/Services/IArticlesService.cs
@@ -1,30 +1,32 @@
 ﻿using NewsTella.Migrations;
+using Microsoft.AspNetCore.Identity;
 using NewsTella.Models.Database;
 
 namespace NewsTella.Services
 {
 	public interface IArticlesService
 	{
-		public void AddArticle(Article article);
-    
+		public void AddArticle(Article article);    
 		public void UpdateArticle(Article article);    
-
         public void UpdateArticleStatus(Article article);
-    
+        public void UpdateArticleStatusPublished(Article article);
+        public void UpdateArticleStatusSubmitted(Article article);
+
         public void DeleteArticle(Article article);
-        public Article GetArticleById(int id);
-             
+
+        public Article GetArticleById(int id);             
         public List<Article> GetArticles();
             
         public ICollection<Article> FindByCategory(string category);
-
         public ICollection<Article> FindByHeadline(string headline);
-        public Task LikeArticleAsync(int id);
-              
+
+        public Task LikeArticleAsync(int id);              
         public Task IncrementViewsAsync(int id);
+
         public Task GetArticleByIdAsync(int id, Article article);
         public Task GetArticleByIdAsync();
         Task<string?> GetArticleByIdAsync(int id);
+       
     }
 
 }
