393201112b7203604c47f3e7424f1450c91b7943
nadee <nadee@NADEE>
2024-06-10 15:38:47 +0200

    bug fixed

--- NewsTella/Services/IArticlesService.cs
+++ NewsTella/Services/IArticlesService.cs
@@ -34,9 +34,7 @@ namespace NewsTella.Services
         public List<Article> GetArticles();
         public List<Article> LatestArticles(int articleCount);
         
-        public ICollection<Article> FindByCategory(string category);
-        public ICollection<Article> FindByHeadline(string headline);
-
+       
         public Task LikeArticleAsync(int id);              
         public Task IncrementViewsAsync(int id);
 
