278faa63cd3af14bf7e69f89d3164ce74338a17f
elgiganten <elgiganten@DESKTOP-T8PMFC9>
2024-06-18 12:40:08 +0200

    admin dashboard

---
+++ NewsTella/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewsTella.Data;
+using NewsTella.Models.ViewModel;
+
+public class AdminController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public AdminController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        var dashboardViewModel = new DashboardViewModel
+        {
+            TotalUsers = _context.Users.Count(),
+            TotalArticles = _context.Articles.Count(),
+            TotalSubscriptions = _context.Subscriptions.Count(),
+            RecentArticles = _context.Articles.OrderByDescending(a => a.DateStamp).Take(6).ToList()
+        };
+
+        return View(dashboardViewModel);
+    }
+}
