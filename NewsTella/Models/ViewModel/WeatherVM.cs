c406ab6ec380fab7a2971c622f9ab5804ba4632d
nadee <nadee@NADEE>
2024-07-02 14:49:19 +0200

    did some changes on mobile views

---
+++ NewsTella/Models/ViewModel/WeatherVM.cs
@@ -0,0 +1,28 @@
+﻿namespace NewsTella.Models.ViewModel
+{
+    public class WeatherVM
+    {
+        public int Temperature { get; set; }
+
+        public int Humidity { get; set;}
+
+        public string Weather { get; set;}
+
+        public double FeelsLike { get; set; }
+
+        public double MinTemperature { get; set; }
+
+        public double MaxTemperature { get; set; }
+
+        public int Pressure { get; set; }
+
+        public string WeatherDescription { get; set; }
+
+        public double WindSpeed { get; set; }
+
+        public int WindDirection { get; set; }
+
+        public int Cloudiness { get; set; }
+
+    }
+}
