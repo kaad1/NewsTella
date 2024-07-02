7f71319c9a4df8ec94c265c99ab393cd52024bcc
nadee <nadee@NADEE>
2024-07-02 16:08:44 +0200

    Added weather ui

--- NewsTella/Models/ViewModel/WeatherVM.cs
+++ NewsTella/Models/ViewModel/WeatherVM.cs
@@ -2,26 +2,19 @@
 {
     public class WeatherVM
     {
+        public string City { get; set; }
+        public string Country { get; set; }
+
         public int Temperature { get; set; }
-
-        public int Humidity { get; set;}
-
-        public string Weather { get; set;}
-
+        public int Humidity { get; set; }
+        public string Weather { get; set; }
         public double FeelsLike { get; set; }
-
         public double MinTemperature { get; set; }
-
         public double MaxTemperature { get; set; }
-
         public int Pressure { get; set; }
-
         public string WeatherDescription { get; set; }
-
         public double WindSpeed { get; set; }
-
         public int WindDirection { get; set; }
-
         public int Cloudiness { get; set; }
 
     }
