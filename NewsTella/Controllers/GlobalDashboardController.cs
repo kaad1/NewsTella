5acfd81f0ccb8afc2289cafdd6035d6f552dd67e
nadee <nadee@NADEE>
2024-07-03 11:16:33 +0200

    Added Electricity Price to Global dashboard

--- NewsTella/Controllers/GlobalDashboardController.cs
+++ NewsTella/Controllers/GlobalDashboardController.cs
@@ -1,32 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using NewsTella.Models.ViewModel;
 using NewsTella.Services;
-using Newtonsoft.Json;
-using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace NewsTella.Controllers
 {
     public class GlobalDashboardController : Controller
     {
-        private readonly HttpClient _httpClient;
         private readonly IWeatherService _weatherService;
+        private readonly ICurrencyExchangeService _currencyService;
+        private readonly IElectricityPriceService _electricityPriceService;
 
-        public GlobalDashboardController(IWeatherService weatherService)
+        public GlobalDashboardController(IWeatherService weatherService, ICurrencyExchangeService currencyService, IElectricityPriceService electricityPriceService)
         {
-            _httpClient = new HttpClient();
             _weatherService = weatherService;
+            _currencyService = currencyService;
+            _electricityPriceService = electricityPriceService;
         }
 
         public async Task<IActionResult> Detail()
         {
-            GlobalVM globalVM = new GlobalVM();
-
             string city = "linköping,401";
-            var weatherVM  = await _weatherService.GetWeatherAsync(city);
-            globalVM.WeatherVM = weatherVM;
+            string baseCurrency = "USD"; // Example base currency
+            string targetCurrency = "SEK"; // Example target currency
+            string country = "SE"; // Example country for electricity prices
 
+            var weatherVM = await _weatherService.GetWeatherAsync(city);
+            var exchangeRateVM = await _currencyService.GetExchangeRateAsync(baseCurrency, targetCurrency);
+            var electricityPriceVM = await _electricityPriceService.GetElectricityPriceAsync(country);
 
+            var globalVM = new GlobalVM
+            {
+                WeatherVM = weatherVM,
+                CurrencyExchangeVM = exchangeRateVM,
+                ElectricityPriceVM = electricityPriceVM
+            };
 
             return View(globalVM);
         }
---
+++ NewsTella/Services/ICurrencyExchangeService.cs
@@ -0,0 +1,9 @@
+﻿using NewsTella.Models.ViewModel;
+
+namespace NewsTella.Services
+{
+    public interface ICurrencyExchangeService
+    {
+        Task<CurrencyExchangeVM> GetExchangeRateAsync(string baseCurrency, string targetCurrency);
+    }
+}
